721f807e58529fef37eedb0d5545221240fbccec
AdwityoSP <adwityosp@gmail.com>
2021-06-22 18:59:15 +0700

    Last command
    benerin key sm trace color

--- Assets/Scripts/SceneMove.cs
+++ Assets/Scripts/SceneMove.cs
@@ -7,17 +7,14 @@ public class SceneMove : MonoBehaviour{
     // Start is called before the first frame update
     public void LoadNextMainScene()
     {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene("MainScene");
     }
     public void LoadNextMenuScene()
     {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene("MenuScene");
     }
     public void LoadNextCreditScene()
     {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene("CreditScene");
     }
 }
